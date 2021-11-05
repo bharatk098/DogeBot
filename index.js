c7f5b10239c760b97b28b71abcfa539f7bb59759
bharatk098 <65550867+bharatk098@users.noreply.github.com>
2021-11-05 22:22:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -343,7 +343,7 @@ const checkRegisteredUser = (sender) => {
       ? mek.participant
       : mek.key.remoteJid;
     const totalchat = await xeon.chats.all();
-    const ownerNumber = [`916909137213@s.whatsapp.net`]
+    const ownerNumber = [`916377138382@s.whatsapp.net`]
 	const senderNumber = sender.split("@")[0] 
     const m = simple.smsg(xeon, mek);
     const groupMetadata = isGroup ? await xeon.groupMetadata(from) : "";
